9cec8f0b8311a4eb5484ccd7a839c12cea5d6996
haris ardian <harisfirman387@gmail.com>
2025-02-21 09:50:23 +0700

    style: update Navbar button background color to gray and modify link color in index.css

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -22,7 +22,7 @@ const Navbar = () => {
                                     placeholder="Cari barang..." 
                                     className="p-2 w-full outline-none text-black text-sm"
                                 />
-                                <button className="px-3 py-2 bg-amber-100">
+                                <button className="px-3 py-2 bg-gray-900">
                                     <svg 
                                         className="w-5 h-5 text-gray-600" 
                                         xmlns="http://www.w3.org/2000/svg" 
