ba129c8696e776dce3488f15c288f9850b9050b4
haris ardian <harisfirman387@gmail.com>
2025-02-25 03:44:52 +0700

    feat: implement product and seller fetching with loading and error handling in Homepage

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,69 +1,75 @@
+import { useContext, useEffect } from 'react';
+import { ProductContext } from '../context/productContext';
+import { SellerContext } from '../context/sellerContext';
 import Navbar from '../components/layouts/Navbar';
 import CardProduk from '../components/fragments/CardProduk';
 import FilterSelect from '../components/fragments/FilterSelect';
 import Footer from '../components/layouts/Footer';
-
+import { formatPrice } from '../utils/function'
+ 
 function Homepage() {
-    return (
-        <>
-            <Navbar/>
-            <FilterSelect/>
-            <div className="w-3/4 pt-10 min-h-screen mx-auto p-5">
-                <h2 className="text-gray-900 pb-2">
-                    Rekomendasi Produk
-                </h2>
-                <div className=' md:flex flex-wrap gap-5 gap-y-6'>
-                    <CardProduk 
-                    foto='https://scontent.fsub8-1.fna.fbcdn.net/v/t39.30808-6/480757095_122139251942513512_4197415608789022688_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeFhoFI2xBSRRFpuzHeYZbJnoSSjPvAvgyOhJKM-8C-DIycwNGZH2n_umDX-A4CszpZVFP_FUlPYOWvYyNI4gIqj&_nc_ohc=jlartmeCTcMQ7kNvgErwA3d&_nc_oc=Adi8osT9xCR4_kUHnQw6sYFc6mw0SgUy20vOrPQZqbeUbpxTG_WCDtwXlAdRTDMXLcGtWd-LoDdlzPb4m040vxKC&_nc_zt=23&_nc_ht=scontent.fsub8-1.fna&_nc_gid=A7pJlTpke_WYqru5gjsQd2I&oh=00_AYC2FFaRi5EFTca0UX5WK_QyY2u6uiqxyR-H1_c5G58FAw&oe=67BBBE7B'
-                    namaProduk='AS'
-                    harga='Rp.10000'
-                    penjual='Bu Sari'
-                    ></CardProduk>
-                    <CardProduk    
-                    foto='https://scontent.fsub8-1.fna.fbcdn.net/v/t39.30808-6/480757095_122139251942513512_4197415608789022688_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeFhoFI2xBSRRFpuzHeYZbJnoSSjPvAvgyOhJKM-8C-DIycwNGZH2n_umDX-A4CszpZVFP_FUlPYOWvYyNI4gIqj&_nc_ohc=jlartmeCTcMQ7kNvgErwA3d&_nc_oc=Adi8osT9xCR4_kUHnQw6sYFc6mw0SgUy20vOrPQZqbeUbpxTG_WCDtwXlAdRTDMXLcGtWd-LoDdlzPb4m040vxKC&_nc_zt=23&_nc_ht=scontent.fsub8-1.fna&_nc_gid=A7pJlTpke_WYqru5gjsQd2I&oh=00_AYC2FFaRi5EFTca0UX5WK_QyY2u6uiqxyR-H1_c5G58FAw&oe=67BBBE7B'
-                    namaProduk='AS'
-                    harga='Rp.10000'
-                    penjual='Bu Sari'
-                    ></CardProduk>
-                     <CardProduk 
-                    foto='https://scontent.fsub8-1.fna.fbcdn.net/v/t39.30808-6/480757095_122139251942513512_4197415608789022688_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeFhoFI2xBSRRFpuzHeYZbJnoSSjPvAvgyOhJKM-8C-DIycwNGZH2n_umDX-A4CszpZVFP_FUlPYOWvYyNI4gIqj&_nc_ohc=jlartmeCTcMQ7kNvgErwA3d&_nc_oc=Adi8osT9xCR4_kUHnQw6sYFc6mw0SgUy20vOrPQZqbeUbpxTG_WCDtwXlAdRTDMXLcGtWd-LoDdlzPb4m040vxKC&_nc_zt=23&_nc_ht=scontent.fsub8-1.fna&_nc_gid=A7pJlTpke_WYqru5gjsQd2I&oh=00_AYC2FFaRi5EFTca0UX5WK_QyY2u6uiqxyR-H1_c5G58FAw&oe=67BBBE7B'
-                    namaProduk='AS'
-                    harga='Rp.10000'
-                    penjual='Bu Sari'
-                    ></CardProduk>
-                    <CardProduk    
-                    foto='https://scontent.fsub8-1.fna.fbcdn.net/v/t39.30808-6/480757095_122139251942513512_4197415608789022688_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeFhoFI2xBSRRFpuzHeYZbJnoSSjPvAvgyOhJKM-8C-DIycwNGZH2n_umDX-A4CszpZVFP_FUlPYOWvYyNI4gIqj&_nc_ohc=jlartmeCTcMQ7kNvgErwA3d&_nc_oc=Adi8osT9xCR4_kUHnQw6sYFc6mw0SgUy20vOrPQZqbeUbpxTG_WCDtwXlAdRTDMXLcGtWd-LoDdlzPb4m040vxKC&_nc_zt=23&_nc_ht=scontent.fsub8-1.fna&_nc_gid=A7pJlTpke_WYqru5gjsQd2I&oh=00_AYC2FFaRi5EFTca0UX5WK_QyY2u6uiqxyR-H1_c5G58FAw&oe=67BBBE7B'
-                    namaProduk='AS'
-                    harga='Rp.10000'
-                    penjual='Bu Sari'
-                    ></CardProduk> <CardProduk 
-                    foto='https://scontent.fsub8-1.fna.fbcdn.net/v/t39.30808-6/480757095_122139251942513512_4197415608789022688_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeFhoFI2xBSRRFpuzHeYZbJnoSSjPvAvgyOhJKM-8C-DIycwNGZH2n_umDX-A4CszpZVFP_FUlPYOWvYyNI4gIqj&_nc_ohc=jlartmeCTcMQ7kNvgErwA3d&_nc_oc=Adi8osT9xCR4_kUHnQw6sYFc6mw0SgUy20vOrPQZqbeUbpxTG_WCDtwXlAdRTDMXLcGtWd-LoDdlzPb4m040vxKC&_nc_zt=23&_nc_ht=scontent.fsub8-1.fna&_nc_gid=A7pJlTpke_WYqru5gjsQd2I&oh=00_AYC2FFaRi5EFTca0UX5WK_QyY2u6uiqxyR-H1_c5G58FAw&oe=67BBBE7B'
-                    namaProduk='AS'
-                    harga='Rp.10000'
-                    penjual='Bu Sari'
-                    ></CardProduk>
-                    <CardProduk    
-                    foto='https://scontent.fsub8-1.fna.fbcdn.net/v/t39.30808-6/480757095_122139251942513512_4197415608789022688_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeFhoFI2xBSRRFpuzHeYZbJnoSSjPvAvgyOhJKM-8C-DIycwNGZH2n_umDX-A4CszpZVFP_FUlPYOWvYyNI4gIqj&_nc_ohc=jlartmeCTcMQ7kNvgErwA3d&_nc_oc=Adi8osT9xCR4_kUHnQw6sYFc6mw0SgUy20vOrPQZqbeUbpxTG_WCDtwXlAdRTDMXLcGtWd-LoDdlzPb4m040vxKC&_nc_zt=23&_nc_ht=scontent.fsub8-1.fna&_nc_gid=A7pJlTpke_WYqru5gjsQd2I&oh=00_AYC2FFaRi5EFTca0UX5WK_QyY2u6uiqxyR-H1_c5G58FAw&oe=67BBBE7B'
-                    namaProduk='AS'
-                    harga='Rp.10000'
-                    penjual='Bu Sari'
-                    ></CardProduk> <CardProduk 
-                    foto='https://scontent.fsub8-1.fna.fbcdn.net/v/t39.30808-6/480757095_122139251942513512_4197415608789022688_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeFhoFI2xBSRRFpuzHeYZbJnoSSjPvAvgyOhJKM-8C-DIycwNGZH2n_umDX-A4CszpZVFP_FUlPYOWvYyNI4gIqj&_nc_ohc=jlartmeCTcMQ7kNvgErwA3d&_nc_oc=Adi8osT9xCR4_kUHnQw6sYFc6mw0SgUy20vOrPQZqbeUbpxTG_WCDtwXlAdRTDMXLcGtWd-LoDdlzPb4m040vxKC&_nc_zt=23&_nc_ht=scontent.fsub8-1.fna&_nc_gid=A7pJlTpke_WYqru5gjsQd2I&oh=00_AYC2FFaRi5EFTca0UX5WK_QyY2u6uiqxyR-H1_c5G58FAw&oe=67BBBE7B'
-                    namaProduk='AS'
-                    harga='Rp.10000'
-                    penjual='Bu Sari'
-                    ></CardProduk>
-                    <CardProduk    
-                    foto='https://scontent.fsub8-1.fna.fbcdn.net/v/t39.30808-6/480757095_122139251942513512_4197415608789022688_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeFhoFI2xBSRRFpuzHeYZbJnoSSjPvAvgyOhJKM-8C-DIycwNGZH2n_umDX-A4CszpZVFP_FUlPYOWvYyNI4gIqj&_nc_ohc=jlartmeCTcMQ7kNvgErwA3d&_nc_oc=Adi8osT9xCR4_kUHnQw6sYFc6mw0SgUy20vOrPQZqbeUbpxTG_WCDtwXlAdRTDMXLcGtWd-LoDdlzPb4m040vxKC&_nc_zt=23&_nc_ht=scontent.fsub8-1.fna&_nc_gid=A7pJlTpke_WYqru5gjsQd2I&oh=00_AYC2FFaRi5EFTca0UX5WK_QyY2u6uiqxyR-H1_c5G58FAw&oe=67BBBE7B'
-                    namaProduk='AS'
-                    harga='Rp.10000'
-                    penjual='Bu Sari'
-                    ></CardProduk>
-                </div>
-            </div>
-            <Footer/>
-        </> 
-    );
+  const { products, loading: loadingProducts, error: errorProducts, getAllProducts } = useContext(ProductContext);
+  const { sellers, getAllSellers } = useContext(SellerContext);
+
+  useEffect(() => {
+    const fetchData = async () => {
+        try {
+            await Promise.all([
+                getAllProducts(),
+                getAllSellers()
+            ]);
+        } catch (error) {
+            console.error('Fetch error:', error);
+        }
+    };
+    
+    if (products.length === 0 && sellers.length === 0) {
+        fetchData();
+    }
+}, []);
+
+  const getSellerName = (sellerId: number) => {
+    const seller = sellers.find(s => s.id === sellerId);
+    return seller ? seller.name : 'Unknown Seller';
+  };
+  
+
+  if (loadingProducts) {
+    return <div className="text-black-900 text-center py-8">Loading products...</div>;
+  }
+
+  if (errorProducts) {
+    return <div className="text-red-500 text-center py-8">Error: {errorProducts}</div>;
+  }
+
+  return (
+    <>
+      <Navbar/>
+      <FilterSelect/>
+      <div className="w-3/4 pt-10 min-h-screen mx-auto p-5">
+        <h2 className="text-gray-900 pb-2">
+          Rekomendasi Produk
+        </h2>
+        
+        {products.length === 0 && !loadingProducts && (
+          <div className="text-center py-4">Tidak ada produk tersedia</div>
+        )}
+
+        <div className='md:flex flex-wrap gap-5 gap-y-6'>
+          {products.map(product => (
+            <CardProduk 
+              key={product.id}
+              foto={typeof product.image === 'string' ? product.image : product.image[0]}
+              namaProduk={product.name}
+              harga={formatPrice(product.price)}
+              penjual={getSellerName(product.sellerId)}
+            />
+          ))}
+        </div>
+      </div>
+      <Footer/>
+    </> 
+  );
 }
 
 export default Homepage;
