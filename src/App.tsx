c02ba6136ba3071fbcccc4bbd71149bb088a7e1d
haris ardian <harisfirman387@gmail.com>
2025-02-25 03:44:13 +0700

    feat: integrate AppProvider and HashRouter into main application structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,24 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Homepage from './pages/Homepage'
-import ResultPage from './pages/ResultPage'
+import { Routes, Route } from "react-router-dom";
+import Homepage from './pages/Homepage';
+import ResultPage from './pages/ResultPage';
 import DetailPage from "./pages/DetailPage";
 import PenjualPage from "./pages/PenjualPage";
 import AuthPage from "./pages/AuthPage";
 import AdminAkunPage from "./pages/AdminAkunPage";
-import AdminProdukPage from "./pages/AdminProdukpage";
-import './App.css'
+import AdminProdukPage from "./pages/AdminProdukPage"; 
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/result" element={<ResultPage />} />
-        <Route path="/detail" element={<DetailPage />} />
-        <Route path="/penjual" element={<PenjualPage />} />
-        <Route path="/auth" element={<AuthPage />} />
-        <Route path="/admin/akun" element={<AdminAkunPage />} />
-        <Route path="/admin/produk" element={<AdminProdukPage />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/result" element={<ResultPage />} />
+      <Route path="/detail" element={<DetailPage />} />
+      <Route path="/penjual" element={<PenjualPage />} />
+      <Route path="/auth" element={<AuthPage />} />
+      <Route path="/admin/akun" element={<AdminAkunPage />} />
+      <Route path="/admin/produk" element={<AdminProdukPage />} />
+    </Routes>
   );
 }
 
-export default App
+export default App;
