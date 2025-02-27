5ff4eb61f4dc081717be22305e0431193704a913
haris ardian <harisfirman387@gmail.com>
2025-02-27 10:27:01 +0700

    code: replace HashRouter with BrowserRouter for improved routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { AppProvider } from './provider/AppProvider.tsx';
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <AppProvider>
         <App />
       </AppProvider>
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>,
 )
