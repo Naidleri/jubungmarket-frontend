6bcf87d16179547ae363c390970aa2304ac4daf9
haris ardian <harisfirman387@gmail.com>
2025-03-07 07:08:12 +0700

    feat: implement protected routes and authentication context in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { AuthProvider } from "./context/authProvider";
 import Homepage from './pages/Homepage';
 import ResultPage from './pages/ResultPage';
 import DetailPage from "./pages/DetailPage";
@@ -7,19 +8,59 @@ import AuthPage from "./pages/AuthPage";
 import AdminAkunPage from "./pages/AdminAkunPage";
 import AdminProdukPage from "./pages/AdminProdukPage"; 
 import AddProductPage from "./pages/AddProductPage";
+import { ReactElement, useContext } from 'react';
+import { AuthContext } from "./context/authContext";
+
+const ProtectedRoute = ({ children }: { children: ReactElement }) => {
+  const { isAuthenticated, loading } = useContext(AuthContext);
+
+  if (loading) {
+    return <div>Loading...</div>; 
+  }
+
+  if (!isAuthenticated) {
+    return <Navigate to="/auth" replace />;
+  }
+
+  return children;
+};
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Homepage />} />
-      <Route path="/result" element={<ResultPage />} />
-      <Route path="/detail/:productId" element={<DetailPage />} />
-      <Route path="/penjual" element={<PenjualPage />} />
-      <Route path="/auth" element={<AuthPage />} />
-      <Route path="/admin/akun" element={<AdminAkunPage />} />
-      <Route path="/admin/produk" element={<AdminProdukPage />} />
-      <Route path="/admin/tambah-produk" element={<AddProductPage />} />
-   </Routes>  
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/result" element={<ResultPage />} />
+        <Route path="/detail/:productId" element={<DetailPage />} />
+        <Route path="/penjual" element={<PenjualPage />} />
+        <Route path="/auth" element={<AuthPage />} />
+
+        <Route 
+          path="/admin/akun" 
+          element={
+            <ProtectedRoute>
+              <AdminAkunPage />
+            </ProtectedRoute>
+          } 
+        />
+        <Route 
+          path="/admin/produk" 
+          element={
+            <ProtectedRoute>
+              <AdminProdukPage />
+            </ProtectedRoute>
+          } 
+        />
+        <Route 
+          path="/admin/tambah-produk" 
+          element={
+            <ProtectedRoute>
+              <AddProductPage />
+            </ProtectedRoute>
+          } 
+        />
+      </Routes>
+    </AuthProvider>
   );
 }
 
