93165b3ae0623a99bfa26cb86259a566f253b0b2
haris ardian <harisfirman387@gmail.com>
2025-03-07 07:08:19 +0700

    feat: add request interceptor to include authorization token in API calls

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,9 +1,25 @@
 import axios from 'axios';
+import Cookies from 'js-cookie';
 
 const api = axios.create({
   baseURL: 'https://juket-id-backend.vercel.app/', 
   timeout: 15000, 
   headers: { 'Content-Type': 'application/json' } 
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const token = Cookies.get("token") || localStorage.getItem("token")
+
+    if (token && ["post","put","delete"].includes(config.method || "")) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+)
 
 export default api;
